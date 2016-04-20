f77fc5beb8549eb3e1182daec05fa2a8c5da20e3
amorvan <antoine.morvan@echoes.fr>
2016-04-20 16:19:01 +0200

    add parent pom; import tests; fix pom.xml files; fix build.properties;

---
+++ org.eclipse.wst.jsdt.core.tests.model/src/org/eclipse/wst/jsdt/core/tests/dom/GetKeyVisitor.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.wst.jsdt.core.tests.dom;
+
+import org.eclipse.wst.jsdt.core.dom.ASTVisitor;
+import org.eclipse.wst.jsdt.core.dom.ClassInstanceCreation;
+import org.eclipse.wst.jsdt.core.dom.IFunctionBinding;
+
+import junit.framework.Assert;
+
+class GetKeyVisitor extends ASTVisitor {
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.wst.jsdt.core.dom.ASTVisitor#visit(org.eclipse.wst.jsdt.core.dom.SimpleName)
+	 */
+	public boolean visit(ClassInstanceCreation node) {
+		IFunctionBinding constructorBinding = node.resolveConstructorBinding();
+		Assert.assertNotNull(constructorBinding);
+		Assert.assertNotNull(constructorBinding.getKey());
+		return super.visit(node);
+	}
+}
---
+++ org.eclipse.wst.jsdt.core.tests.model/src/org/eclipse/wst/jsdt/core/tests/utils/SequenceReaderTests.java
@@ -0,0 +1,104 @@
+/*******************************************************************************
+ * Copyright (c) 2015 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.wst.jsdt.core.tests.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
+import org.eclipse.wst.jsdt.core.util.SequenceReader;
+
+import junit.framework.TestCase;
+
+public class SequenceReaderTests extends TestCase {
+	public void testSingleReaderSequence() throws IOException {
+		Reader[] readers = new Reader[]{new StringReader("Ready")};
+		Reader sequenceReader = new SequenceReader(readers);
+		StringBuffer buff = new StringBuffer();
+		try {
+			int c = sequenceReader.read();
+			while (c != -1) {
+				buff.append((char) c);
+				c = sequenceReader.read();
+			}
+		} finally {
+			sequenceReader.close();
+		}
+		assertEquals("Ready", buff.toString());
+	}
+
+	public void testTripleReaderSequence() throws IOException {
+		Reader[] readers = new Reader[]{new StringReader("Ready"), new StringReader("Set"), new StringReader("Go")};
+		Reader sequenceReader = new SequenceReader(readers);
+		StringBuffer buff = new StringBuffer();
+		try {
+			int c = sequenceReader.read();
+			while (c != -1) {
+				buff.append((char) c);
+				c = sequenceReader.read();
+			}
+		} finally {
+			sequenceReader.close();
+		}
+		assertEquals("ReadySetGo", buff.toString());
+	}
+
+	public void testEmptyReader() throws IOException {
+		Reader[] readers = new Reader[]{new StringReader("")};
+		Reader sequenceReader = new SequenceReader(readers);
+		StringBuffer buff = new StringBuffer();
+		try {
+			int c = sequenceReader.read();
+			while (c != -1) {
+				buff.append((char) c);
+				c = sequenceReader.read();
+			}
+		} finally {
+			sequenceReader.close();
+		}
+		assertEquals("something was read from an empty reader", 0, buff.length());
+	}
+
+	public void testEmptySequence() throws IOException {
+		Reader[] readers = new Reader[0];
+		Reader sequenceReader = new SequenceReader(readers);
+		StringBuffer buff = new StringBuffer();
+		try {
+			int c = sequenceReader.read();
+			while (c != -1) {
+				buff.append((char) c);
+				c = sequenceReader.read();
+			}
+		} finally {
+			sequenceReader.close();
+		}
+		assertEquals("something was read from nothing", 0, buff.length());
+	}
+
+	public void testBufferedTripleReaderSequence() throws IOException {
+		Reader[] readers = new Reader[]{new StringReader("Ready"), new StringReader("Set"), new StringReader("Go")};
+		Reader sequenceReader = new BufferedReader(new SequenceReader(readers));
+		StringBuffer buff = new StringBuffer();
+		try {
+			int c = sequenceReader.read();
+			while (c != -1) {
+				buff.append((char) c);
+				c = sequenceReader.read();
+			}
+		} finally {
+			sequenceReader.close();
+		}
+		assertEquals("ReadySetGo", buff.toString());
+	}
+
+}
